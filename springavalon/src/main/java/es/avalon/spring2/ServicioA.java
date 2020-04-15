6cd6f166b314568b57fdefc56d33b205b48bb80b
Razvan Pascal <razvanpascal@outlook.com>
2020-04-15 21:24:54 +0200

    Proyecto Spring

---
+++ springavalon/src/main/java/es/avalon/spring2/ServicioA.java
@@ -0,0 +1,13 @@
+package es.avalon.spring2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioA {
+	
+	public String mensaje() {
+		
+		return "Hola desde Spring";
+	}
+
+}
