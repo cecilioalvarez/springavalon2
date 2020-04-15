82044baa4205059a3b95d1613499d715c8a29432
Oliver <Oliver@MSI>
2020-04-15 14:00:39 +0200

    Spring2

---
+++ springavalon/src/main/java/es/avalon/spring2/ServicioA.java
@@ -0,0 +1,11 @@
+package es.avalon.spring2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioA {
+	public String mensaje() {
+		return "hola desde spring";
+	}
+
+}
---
+++ springavalon/src/main/java/es/avalon/spring2/SpringConfigurador.java
@@ -0,0 +1,11 @@
+package es.avalon.spring2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("es.avalon.spring2")
+public class SpringConfigurador {
+	
+
+}
