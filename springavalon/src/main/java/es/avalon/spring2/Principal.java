382c8fc1e526908aad66bcedcd86701de23b07ad
Formacion <Formacion@192.168.0.30>
2020-04-15 14:00:52 +0200

    15/4 fin clase

---
+++ springavalon/src/main/java/es/avalon/spring2/Principal.java
@@ -0,0 +1,17 @@
+package es.avalon.spring2;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AnnotationConfigApplicationContext factoria= new AnnotationConfigApplicationContext();
+
+factoria.register(SpringConfigurador.class);
+factoria.refresh();
+ServicioA miservicio= factoria.getBean(ServicioA.class);
+System.out.println(miservicio.mensaje());
+	}
+
+}
