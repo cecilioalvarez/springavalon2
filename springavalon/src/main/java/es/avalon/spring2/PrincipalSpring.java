1b652e2158a1342b15ac3af827fa9ce986760303
Formacion <Formacion@5CD9494ZLKC>
2020-04-15 14:01:19 +0200

    Inicio en SpringFramwork

---
+++ springavalon/src/main/java/es/avalon/spring2/PrincipalSpring.java
@@ -0,0 +1,16 @@
+package es.avalon.spring2;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PrincipalSpring {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext factoria = new AnnotationConfigApplicationContext();
+		factoria.register(SpringConfigurador.class);
+		factoria.refresh();
+		ServicioA miServicio = factoria.getBean(ServicioA.class);
+		System.out.println(miServicio.mensaje());
+		
+	}
+}
