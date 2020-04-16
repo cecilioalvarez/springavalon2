4dc9bd533764730c9475435dbd6886f8ce90993f
cursos <cursos@localhost>
2020-04-16 12:04:58 +0200

    modificacioens spring 4

--- springavalon/src/main/java/es/avalon/spring4/PrincipalSpring.java
+++ springavalon/src/main/java/es/avalon/spring4/PrincipalSpring.java
@@ -1,8 +1,12 @@
 package es.avalon.spring4;
 
+import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import es.avalon.dominio.Libro;
+import es.avalon.repositorios.LibroRepository;
+import es.avalon.repositorios.jpa.LibroRepositoryJPA;
 
 public class PrincipalSpring {
 
@@ -11,10 +15,13 @@ public class PrincipalSpring {
 		AnnotationConfigApplicationContext factoria = new AnnotationConfigApplicationContext();
 		factoria.register(SpringConfigurador.class);
 		factoria.refresh();
+		LibroRepository repo = factoria.getBean(LibroRepository.class);
+		List<Libro> lista = repo.buscarTodos();
 
-	
-	
+		for (Libro libro : lista) {
 
+			System.out.println(libro.getTitulo());
+		}
 	}
 
 }
